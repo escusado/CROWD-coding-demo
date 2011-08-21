d3ebd3912c5fe0e572effc9d1dbb571cce8de40c
joaquin benitez <Toily@.(none)>
2011-08-21 04:12:06 -0500

    added css for header and columns, yepnope after all

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*set all columns height to the highest one*/
+$('.adjust-height').each(function(){
+	var max_height = 0;
+	$(this).children().each(function(){
+		max_height = ( $(this).height() > max_height ) ? $(this).height() : max_height;
+	});
+	$(this).children().height(max_height);
+});
+
