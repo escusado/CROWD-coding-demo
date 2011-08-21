d108471f9d0e4086113938bad6e9a085764e8401
joaquin benitez <Toily@.(none)>
2011-08-21 18:04:21 -0500

    addedd slideshow init and slide values

---
+++ js/crowd_slideshow.js
@@ -0,0 +1,64 @@
+/*CONFIG*/
+var hotspot = 68; //more slides less hotspot width
+
+(function( $ ){
+
+	var methods = {
+		init : function() {
+
+			$(this).find('.slide').each(function(){
+				$(this).css('left',$(this).crowd_slide('left_pos'));
+			});
+
+			$(this).find('.slide').delay(500).fadeIn('slow');
+		}
+	};
+
+  $.fn.crowd_slideshow = function( method ) {
+    
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + 'error' );
+    }    
+  
+  };
+
+})( jQuery );
+
+/*SLIDE POSITION*/
+(function( $ ){
+
+	var methods = {
+		//assign each slide its left and right position based on the hotspot area and the width of the slideshow container
+
+		left_pos : function() {
+			var slide_index = $(this).attr('id').replace('slide-','');
+			return (hotspot * slide_index)+'px';
+		},
+		right_pos : function() {
+			//refactor into less operations
+			var slideshow_width = $(this).parent().width();
+			var number_of_slides = $(this).parent().find('.slide').length;		
+			var right_margin = slideshow_width - (number_of_slides * hotspot);
+			var slide_index = $(this).attr('id').replace('slide-','');
+
+			return (hotspot * slide_index + right_margin)+'px';
+		}
+	};
+
+  $.fn.crowd_slide = function( method ) {
+    
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + 'error' );
+    }    
+  
+  };
+
+})( jQuery );
--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 /*set all columns height to the highest one*/
-$('.adjust-height').each(function(){
-	var max_height = 0;
-	$(this).children().each(function(){
-		max_height = ( $(this).height() > max_height ) ? $(this).height() : max_height;
+$(document).ready(function(){
+	$('.adjust-height').each(function(){
+		var max_height = 0;
+		$(this).children().each(function(){
+			max_height = ( $(this).height() > max_height ) ? $(this).height() : max_height;
+		});
+		$(this).children().height(max_height);
 	});
-	$(this).children().height(max_height);
 });
-
