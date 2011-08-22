481f1258d7f6790d4176648b60142459f865e61e
joaquin benitez <Toily@.(none)>
2011-08-22 04:11:35 -0500

    feedback form complete :D and... well all done

--- js/crowd_feedback.js
+++ js/crowd_feedback.js
@@ -7,6 +7,7 @@
 			var page_document = $(this);
 			
 			//trigger hover animation
+			//a wierd bug is going on with this animation, sometimes it messes with the slideshow animation timer :\
 			$('#crowd-feedback-trigger').hover(
 				function(){
 					$(this).animate({left:-10});
@@ -22,6 +23,10 @@
 
 			$('#crowd-feedback-cancel').click(function(){
 				page_document.crowd_feedback('hide_feedback_form');
+			});
+
+			$('#submit').click(function(){
+				page_document.crowd_feedback('submit');
 			});
 
 		},
@@ -42,8 +47,38 @@
 		hide_feedback_form : function(){
 			$('body').css('overflow', 'auto');
 			$('#lightbox').fadeOut();
-
 			$('#crowd-feedback-container').fadeOut();
+		},
+		submit : function(){
+			var page_document = $(this);
+
+			//try to submit form
+			//here we will do everything to make sure the
+			//form is correct, client side validation with jQuery validate (of course he!)
+			//ajax call to a server side script that validates and sends the mail or whatever the
+			//script does and returns an "OK" beacon so we can trigger the necesary procedure
+			//I know how to do all of that http://code.chirango.com/2011/05/ajax-contact-form/ :)
+
+			//so if the mail was succesfully sent...
+
+			$('#crowd-feedback-envelope-front, #crowd-feedback-envelope-back').css('left', ($(window).width() - $('#crowd-feedback-envelope-front').width()) / 2); 
+			var envelope_top_pos = ($(window).height() - $('#crowd-feedback-envelope-front').height()) / 2 ;
+			$('#crowd-feedback-envelope-front').css('bottom', $('#crowd-feedback-envelope-front').height() * -1 );
+			$('#crowd-feedback-envelope-back').css('bottom', $('#crowd-feedback-envelope-back').height() * -1 );
+
+			$('#crowd-feedback-envelope-front, #crowd-feedback-envelope-back').show();
+
+			//position fine tuned and hardcoded for now :(
+			envelope_top_pos = 110;
+
+			//animate entrance
+			$('#crowd-feedback-envelope-front, #crowd-feedback-envelope-back').animate({bottom:envelope_top_pos},{duration:1000});
+			
+			//and of you go!
+			$('#crowd-feedback-envelope-front, #crowd-feedback-envelope-back').animate({left:2000},{duration:1000});
+			$('#crowd-feedback-container').delay(1000).animate({left:2000},1000,function(){
+				page_document.crowd_feedback('hide_feedback_form');
+			});
 		}
 	};
 
